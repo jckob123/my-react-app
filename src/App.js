0401e294a0bae137e7febf627c016ea2def92e0e
jckob123 <44051651+jckob123@users.noreply.github.com>
2018-12-12 21:30:52 +0100

    navigation added
    to-do

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from '../src/components/navigation/navigation.js';
+import Navigation from './components/Navigation/Navigation.js';
 import Title from '../src/components/title/title.js';
 import { Container, Row, Col } from 'reactstrap';
 
@@ -8,18 +8,20 @@ class App extends Component {
   render() {
     return ( 
     <Container>
-      <Col lg="2" sm="12" className="">
-        <Title></Title>            
-      </Col>    
       <Row>
-        <Col>
+        <Col lg="2" sm="12" className="">
+          <Title></Title>            
+        </Col>    
+      </Row>
+      <Row>
+        <Col className="navigator justify-content-center">
           <Navigation></Navigation>  
         </Col>
+        
         <Col>
         
         </Col>
-      </Row>     
-      <Navigation></Navigation>
+      </Row>
     </Container>
     )
   }
--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Nav } from 'reactstrap';
+import '../Navigation/navigation.css';
+
 
 class Navigation extends Component {
   render() {
     return (
-    <div className="navigation-wrapper">
-        <ListGroup>
-            <ListGroupItem className="list-group-item list-i">
-            <a className="llink" href="#">About</a>
-            </ListGroupItem>
-            <ListGroupItem className="list-group-item list-i ">
-            Contact
-            </ListGroupItem>
-        </ListGroup>
-    </div>
-
+      <BrowserRouter className="navigation-wrapper">
+        <Nav>
+          <ul className="list-group">
+            <li className="list-group-item list-i">
+              <Link to="/About" style={{ textDecoration: 'none'}}>About</Link>
+            </li>
+            <li className="list-group-item list-i">
+              <Link to="/Gallery" style={{ textDecoration: 'none',color:"white"}}>Gallery</Link>
+            </li>
+            <li className="list-group-item list-i">
+              <Link to="/Contact"
+                    activeClassName="nounderline"
+                    activeStyle={{textDecoration: 'none'}}>Contact</Link>
+            </li>
+          </ul>
+        </Nav>
+      </BrowserRouter>
     )
   };
 }
